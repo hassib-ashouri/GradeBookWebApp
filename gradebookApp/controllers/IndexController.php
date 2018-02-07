ea21bd16eb02b52051f2dcfcf913e01051cb8230
Mitchell LeMoine <whitefang573@gmail.com>
2018-02-06 18:26:18 -0800

    init: codeIgniter hello world

---
+++ gradebookApp/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class IndexController extends MY_Controller
+{
+    public function index()
+    {
+        echo "Hello world!";
+    }
+}
---
+++ gradebookApp/helpers/utility_helper.php
@@ -0,0 +1,71 @@
+<?php
+
+if (!function_exists('asset_url')) {
+    /**
+     * Gets the absolute url to the assets directory
+     * @return string
+     */
+    function asset_url()
+    {
+        return base_url() . 'assets/';
+    }
+}
+
+if (!function_exists('s4')) {
+    /**
+     * Generates a 4 digit hex number as a string
+     * @return string
+     */
+    function s4()
+    {
+        $rand = rand(65536, 131071);
+        $hex = dechex(floor($rand));
+        return substr($hex, 1);
+    }
+}
+
+if (!function_exists('guid')) {
+    /**
+     * Generates a unique(ish) id
+     *      credit: https://stackoverflow.com/a/105074
+     * @return string
+     */
+    function guid()
+    {
+        return s4() . s4() . '-' . s4() . '-' . s4() . '-' .
+            s4() . '-' . s4() . s4() . s4();
+    }
+}
+
+if (!function_exists('images_url')) {
+    /**
+     * Gets the absolute url to the assets/images directory
+     * @return string
+     */
+    function images_url()
+    {
+        return asset_url() . 'images/';
+    }
+}
+
+if (!function_exists('javascripts_url')) {
+    /**
+     * Gets the absolute url to the assets/javascripts directory
+     * @return string
+     */
+    function javascripts_url()
+    {
+        return asset_url() . 'javascripts/';
+    }
+}
+
+if (!function_exists('stylesheets_url')) {
+    /**
+     * Gets the absolute url to the assets/stylesheets directory
+     * @return string
+     */
+    function stylesheets_url()
+    {
+        return asset_url() . 'stylesheets/';
+    }
+}
