72814a5a407e70b09ff076c8146c422a1884236d
Hassib Ashouri <hassib291@hotmail.com>
2018-03-30 04:38:03 -0700

    feature: the header can be fed the names of javascript files to be loaded for the view

--- gradebookApp/views/header.php
+++ gradebookApp/views/header.php
@@ -12,36 +12,44 @@ if (!isset($stylesheets)) {
 }
 ?>
 <!DOCTYPE html>
-    <head>
-        <link rel="icon" href="<?= images_url() ?>favicon.png" type="image/png"/>
-        <title><?= $title ?></title>
+<head>
+    <link rel="icon" href="<?= images_url() ?>favicon.png" type="image/png"/>
+    <title><?= $title ?></title>
+
+    <!-- Hassib- added the bootstrap libraries -->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            crossorigin="anonymous"></script>
+
+    <!--<script type="text/javascript" src="-->
+    <? //= javascripts_url() ?><!--main.js" defer></script>-->
+    <?php foreach ($javascripts as $jsUrl): ?>
+        <script type="text/javascript" src="<?= javascripts_url() . $jsUrl ?>" defer></script>
+    <?php endforeach ?>
+
+    <!--    <link rel="stylesheet" type="text/css" href="-->
+    <? //= stylesheets_url() ?><!--styles.css">-->
+    <?php foreach ($stylesheets as $cssUrl): ?>
+        <link rel="stylesheet" type="text/css" href="<?= stylesheets_url() . $cssUrl ?>">
+    <?php endforeach ?>
+    <script>
+        var IDGF = {};
+        IDGF.baseURL = '<?= base_url(); ?>';
+    </script>
+</head>
 
 
-        <!-- Hassib- added the bootstrap libraries -->
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-
-         <!--<script type="text/javascript" src="--><?//= javascripts_url() ?><!--main.js" defer></script>-->
-        <?php foreach ($javascripts as $jsUrl): ?>
-            <script type="text/javascript" src="<?= javascripts_url() . $jsUrl ?>" defer></script>
-        <?php endforeach ?>
-
-        <!--    <link rel="stylesheet" type="text/css" href="--><?//= stylesheets_url() ?><!--styles.css">-->
-        <?php foreach ($stylesheets as $cssUrl): ?>
-            <link rel="stylesheet" type="text/css" href="<?= stylesheets_url() . $cssUrl ?>">
-        <?php endforeach ?>
-    </head>
-
-
-
-
-    <div class="container-fluid bg-primary" style="">
-
-        <div class="h1">IDGF</div>
-        <div class="h5">the user name could go here</div>
-
-    </div>
+<div class="container-fluid bg-primary" style="">
+    <div class="h1">IDGF</div>
+    <div class="h5">the user name could go here</div>
+</div>
 
 
 <meta charset="UTF-8">
