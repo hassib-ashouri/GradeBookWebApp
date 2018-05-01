1dc143bbc1f3ae4a2be0cb12daf5616cf24575ac
Mitchell LeMoine <whitefang573@gmail.com>
2018-04-30 21:42:02 -0700

    feat: add _aliasAssignmentName function to Class_controller
    -can be used when creating the main class table

--- gradebookApp/controllers/Class_controller.php
+++ gradebookApp/controllers/Class_controller.php
@@ -56,6 +56,59 @@ class Class_controller extends MY_Controller
      */
 
     /**
+     * todo remove if not needed
+     */
+    public function testAlias()
+    {
+        $tableName = "class_29506_SE-131_02_table";
+        $this->load->model("class_model");
+        $classObj = $this->class_model->getClass($tableName);
+
+        foreach ($classObj->getAssignments() as $assignment) {
+            pretty_dump($assignment->assignment_name);
+            $this->_aliasAssignmentName($assignment->assignment_name);
+        }
+    }
+
+    /**
      * Private methods
      */
+
+    /**
+     * Transforms an $assignmentName into something more 'table-friendly';
+     *      maximum length of 4 characters
+     * @param string $assignmentName
+     * @return string
+     */
+    private function _aliasAssignmentName($assignmentName)
+    {
+        $ALIAS_LENGTH = 4;
+
+        $matches = array();
+        $pattern = '/(?:(\b.)\w*?\s)(?:(\b.)\w*?\s)?(?:(\b.)\w*?\s)?(?:(\b.)\w*?\s)?(\d*)/';
+        preg_match($pattern, $assignmentName, $matches);
+        $alias = '';
+
+        // try with regex
+        if (isset($matches[5])) {
+            $number = $matches[5];
+            $numberLength = strlen($number);
+        } else {
+            $number = '';
+            $numberLength = 0;
+        }
+        for ($i = 1; $i < $ALIAS_LENGTH - $numberLength + 1; $i++) {
+            if (isset($matches[$i])) {
+                $alias .= $matches[$i];
+            }
+        }
+        $alias .= $number;
+
+        // try with first char only
+        if (strlen($alias) == 0) {
+            $alias = substr($assignmentName, 0, 1);
+        }
+
+        return $alias;
+    }
 }
