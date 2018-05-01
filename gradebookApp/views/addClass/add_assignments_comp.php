55d9ec43102d24062eb969325fdaa017b97efb06
Hassib Ashouri <hassib291@hotmail.com>
2018-04-30 20:59:51 -0700

    add the message to warn that sum of weights should be a hundred.

--- gradebookApp/views/addClass/add_assignments_comp.php
+++ gradebookApp/views/addClass/add_assignments_comp.php
@@ -2,17 +2,15 @@
 ?>
 
 <div class="container" style="border-color : black">
-    <h3> Add Assignments:</h3>
+    <h3> Add Assignments: <button id="addgroupbtn" type="button" class="btn">Add Group</button></h3>
+    <p class="text-muted fornt-weight-light">Note: the sum of all the weights should equal to 100.</p>
 
     <ul id="groupsList" class="list-group">
 
     </ul>
     <div class="container mt-2">
-        <div class="row">
-             <button id="addgroupbtn" class="btn">Add Group</button>
-        </div>
         <div class="row mt-4">
-            <button id="getInfo" class="btn">getInfo</button>
+            <button id="addClass" class="btn">Add Class</button>
         </div>
     </div>
 </div>
