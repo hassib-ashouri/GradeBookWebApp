63434d4abd6ac3af7b6a3dbb6451f6617da13f50
Hassib Ashouri <hassib291@hotmail.com>
2018-05-06 17:52:50 -0700

    The partail view to show the grades for student, but editing the grades yet.

--- gradebookApp/views/class/main/detailed.php
+++ gradebookApp/views/class/main/detailed.php
@@ -1,6 +1,52 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * @var String[] the names of the assignments.
+ */
+$assignmentsNames = isset($assignmentsNames) ? $assignmentsNames : array("no data");
+/**
+ * @var array[] where the keys are the names of students and the values are arrays of grades.
+ */
+$grades = isset($grades) ? $grades : array();
 ?>
 
 
 <span>This is the component where the detailed grades for the students to show.</span>
+
+
+<h4>Students' Grades</h4>
+
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">
+            Student Name
+        </th>
+        <?php foreach ($assignmentsNames as $index => $assignmentName): ?>
+            <th scope="col">
+                <?=$assignmentName ?>
+            </th>
+        <?php endforeach; ?>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach($grades as $studentName => $gradeArr): ?>
+    <tr>
+
+        <th scope="row">
+            <?= $studentName ?>
+        </th>
+
+        <?php foreach($gradeArr as $grade):?>
+            <td>
+                <?= $grade ?>
+            </td>
+        <?php endforeach;?>
+    </tr>
+    <?php endforeach; ?>
+
+    </tbody>
+
+
+</table>
