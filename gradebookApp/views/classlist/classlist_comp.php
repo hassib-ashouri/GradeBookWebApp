44b85408619b15c69d478fa8b259d3fe072218ec
Mitchell LeMoine <whitefang573@gmail.com>
2018-05-10 14:01:07 -0700

    feat: classlist alignment using flexbox

--- gradebookApp/views/classlist/classlist_comp.php
+++ gradebookApp/views/classlist/classlist_comp.php
@@ -13,21 +13,27 @@ $classObjects = (isset($classObjects)) ? $classObjects : array();
     <ul class="list-group">
         <?php
         foreach ($classObjects as $classObj):?>
-            <li class="list-group-item">
-                <a href="<?= base_url() . 'Class_controller/displayClassInfo/' . $classObj->table_name ?>">
-                    <?= "$classObj->class_name-$classObj->section: $classObj->class_title" ?>
-                </a>
-                <button type="button" class="btn">
-                    <a href="<?= base_url() . 'Edit_class_controller/editClassView/' . $classObj->class_id ?>">
-                        Edit
+            <li class="list-group-item d-flex flex-row">
+                <div class="align-self-center mr-auto">
+                    <a href="<?= base_url() . 'Class_controller/displayClassInfo/' . $classObj->table_name ?>">
+                        <?= "$classObj->class_name-$classObj->section: $classObj->class_title" ?>
                     </a>
-                </button>
-                <button type="button" class="btn btn-danger deleteClassButton" data-html="true"
-                        data-container="body" data-toggle="popover" data-trigger="focus"
-                        data-placement="top" title="Are you sure you want to delete the class?"
-                        data-content="<div style='text-align: center'><button class='btn' type='button' style='margin-right: 1em'><a href='<?= base_url() . 'Class_list_controller/deleteClass/' . $classObj->table_name ?>'>Yes</a></button><button class='btn' type='button'><a href='#'>No</a></button></div>">
-                    Delete
-                </button>
+                </div>
+                <div class="mr-1">
+                    <button type="button" class="btn">
+                        <a href="<?= base_url() . 'Edit_class_controller/editClassView/' . $classObj->class_id ?>">
+                            Edit
+                        </a>
+                    </button>
+                </div>
+                <div>
+                    <button type="button" class="btn btn-danger deleteClassButton" data-html="true"
+                            data-container="body" data-toggle="popover" data-trigger="focus"
+                            data-placement="top" title="Are you sure you want to delete the class?"
+                            data-content="<div style='text-align: center'><button class='btn' type='button' style='margin-right: 1em'><a href='<?= base_url() . 'Class_list_controller/deleteClass/' . $classObj->table_name ?>'>Yes</a></button><button class='btn' type='button'><a href='#'>No</a></button></div>">
+                        Delete
+                    </button>
+                </div>
             </li>
         <?php endforeach; ?>
     </ul>
