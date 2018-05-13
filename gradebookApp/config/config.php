b92a96a232fd90e783a8497e60f175039da2811f
Mitchell LeMoine <whitefang573@gmail.com>
2018-05-12 18:25:44 -0700

    feat: switch live to 'production' mode (no error reporting)

--- gradebookApp/config/config.php
+++ gradebookApp/config/config.php
@@ -23,10 +23,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://cmpe131.trybabynamer.com/';
 switch (ENVIRONMENT)
 {
     case 'development':
+    case 'testing':
+    case 'production':
         $config['base_url'] = 'http://cmpe131.trybabynamer.com/';
         break;
     case 'hassib':
